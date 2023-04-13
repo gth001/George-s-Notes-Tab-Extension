241b24bd3662e9f895563a27aa39afa2384cb9bb
gth001 <gth001@gmail.com>
2023-04-13 11:46:31 -0700

    added indent preservation on new lines

--- 1.0_0/index.js
+++ 1.0_0/index.js
@@ -2,11 +2,34 @@
 document.getElementById('js-note-container').addEventListener('keydown', function(event) {
   //detect 'tab' key
   if(event.key === 'Tab'){
-     //prevent focusing on next element
-    event.preventDefault()   
+    //prevent focusing on next element
+    event.preventDefault();
     //add tab
     document.execCommand('insertHTML', false, '&#009');
+  }
 
+});
+
+// On ENTER, matches tab indent of previous line
+document.getElementById('js-note-container').addEventListener('keydown', function(event) {
+  if(event.key === 'Enter') {
+    event.preventDefault();
+    let cursorPosition = window.getSelection().getRangeAt(0).startOffset;
+    let textBeforeCursor = this.innerText.substring(0, cursorPosition);
+    let lastNewLineIndex = textBeforeCursor.lastIndexOf('\n');
+    let previousLineText = textBeforeCursor.substring(lastNewLineIndex+1);
+    let tabCount = previousLineText.match(/^\t*/)[0].length;
+
+    if(previousLineText.length > 0 && tabCount === 0) {
+      document.execCommand('insertHTML', false, '\n');
+      document.execCommand('insertHTML', false, '\n');
+    } else if(previousLineText.length === 0 || tabCount > 0) {
+      document.execCommand('insertHTML', false, '\n');
+    }
+
+    for(let i = 0; i < tabCount; i++) {
+      document.execCommand('insertHTML', false, '&#009');
+    }
   }
 });
 
